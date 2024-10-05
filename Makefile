77124ef7ba3a5156e16b16458a7a190385aec719
jackr276 <jack_m_robbins@me.com>
2024-10-05 19:43:15 -0400

    Initial setup compiling

--- Makefile
+++ Makefile
@@ -4,16 +4,16 @@
 
 CC = gcc
 PROGS = mempool_demo
-CFLAGS = -Wall -Wextra
+CFLAGS = -pthread -Wall -Wextra
 # You need to pull in mempool to the compilation
-INCLUDE = ./src/mempool/mempool.c
+INCLUDE = ./src/mempool/mempool.c ./src/demo-program/npuzzle/puzzle/puzzle.c ./src/demo-program/npuzzle/solver/solve_multi_threaded.c
 # Compilation directory
 OUT = ./out
 
 all: $(PROGS)
 
-mempool_demo:
-	$(CC) $(CFLAGS) ./src/main.c $(INCLUDE) -o $(OUT)/test
+mempool_demo: # FIXME
+	$(CC) $(CFLAGS) ./src/demo-program/demo.c $(INCLUDE) -o $(OUT)/test
 
 test:
 	$(OUT)/test
---
+++ src/demo-program/demo.c
@@ -0,0 +1,35 @@
+/**
+ * Author: Jack Robbins
+ * This C file contains code to run the demo N-Puzzle solver program for mempool
+ */
+
+
+/**
+ * Makes the need calls to the solver to generate a random puzzle and solve
+ */
+#include <stdio.h>
+#include <sys/types.h>
+
+//Link to the NPuzzle solver and puzzle generator
+#include "./npuzzle/puzzle/puzzle.h"
+#include "./npuzzle/solver/solve.h"
+
+
+/**
+ * Get appropriate user input and make calls to the needed APIs in puzzle and solver
+ */
+int main(void){
+	//Store N and the comlexity
+	u_int32_t N;
+	u_int32_t complexity;
+
+	//Grab N from the user
+	printf("Enter a value for N: ");
+	scanf("%u", &N);
+
+	//Grab the intial complexity
+	printf("Enter the initial complexity: ");
+	scanf("%u", &complexity);
+
+	printf("Generating an N-Puzzle with N = %u and initial complexity = %u\n", N, complexity);
+}
